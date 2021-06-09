f809fea478e28fd6d6b2240aa9d6a8bb02c238f0
tomoki <tomoki_1230@yahoo.co.jp>
2021-06-09 23:47:51 +0900

    「いいね機能」用のルーティング・コントローラを作成

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   validates :name, presence: true
   mount_uploader :image, ImageUploader
 end
